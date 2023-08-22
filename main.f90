e2c6f9221d95b65752ff5c82f7a4330c06ef1b33
Code <59381987+c-o-de@users.noreply.github.com>
2023-08-22 09:09:53 +0600

    Add files via upload

---
+++ main.f90
@@ -0,0 +1,11 @@
+! This is a comment in Fortran. It's ignored by the Fortran compiler and used for documentation.
+
+PROGRAM HelloWorld
+    ! Declare a character variable to store the message
+    CHARACTER(20) :: message = "Hello, Fortran!"
+
+    ! Print the message to the console
+    PRINT *, message
+
+    ! End the program
+    END PROGRAM HelloWorld
